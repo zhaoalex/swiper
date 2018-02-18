5e25d1a7f38324aec02282ba14c2184b2643a51b
Alex Zhao <alexzhao21@gmail.com>
2018-02-17 16:25:39 -0800

    Update LoginViewController.swift

--- Swiper_iOS/LoginViewController.swift
+++ Swiper_iOS/LoginViewController.swift
@@ -12,7 +12,7 @@ import FirebaseAuthUI
 import FBSDKCoreKit
 import FBSDKLoginKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     //@IBOutlet weak var loginButton: FBSDKButton!
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,6 +26,7 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
         let loginButton = FBSDKLoginButton()
+        loginButton.delegate = self
         loginButton.center = view.center
         
         self.view.addSubview(loginButton)
@@ -35,6 +36,26 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
 
-
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if let error = error {
+                // ...
+                return
+            }
+            // User is signed in
+            let newViewController = ListingTableViewCell()
+            self.navigationController?.pushViewController(newViewController, animated: true)
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+    }
 }
